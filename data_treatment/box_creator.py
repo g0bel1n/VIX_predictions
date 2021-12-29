d679e90ea99ac7aeea1d2cf99d722f5a1b92b246
Thomas Kientz <thomas@kientz.net>
2021-12-29 11:20:48 +0100

    binary box, logit

--- data_treatment/box_creator.py
+++ data_treatment/box_creator.py
@@ -29,3 +29,18 @@ def create_box(df, threshold = 2., box_length = 7):
 
     df = df.drop(columns = ["VIX_sup_" + str(i) for i in range(box_length)] + ["VIX_inf_" + str(i) for i in range(box_length)])
     return df
+
+def create_binary_box(df, threshold = 2., box_length = 7):
+    for i in range(box_length):
+        df["VIX_sup_" + str(i)] = (df[VIX].shift(i) > df[VIX] + threshold) # Est-ce que le i-ème jour est au dessus du threshold haut ?
+
+    # Initialisation : Est-ce que le dès le lendemain, on dépasse (1), on est en dessous (-1) ou on est dans la boite (0) ?
+    df["Box"] = 1 * df["VIX_sup_1"]
+
+
+    for i in range(2, box_length):
+        # Iterations : Si la valeur n'est pas encore sortie (Box == 0), sort-elle par le haut (1), bas (-1) ou pas du tout (0) de la boite au jour i ?
+        df["Box"] = (df["Box"] == 0) * (1 *df[f"VIX_sup_{i}"]) + (df["Box"] != 0) * df["Box"]
+
+    df = df.drop(columns = ["VIX_sup_" + str(i) for i in range(box_length)])
+    return df
